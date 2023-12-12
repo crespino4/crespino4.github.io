b425c1d436f5af243102513b46883af0d22394d3
Jim Crespino <jim.crespino@genesys.com>
2023-12-12 09:51:59 -0600

    Added Date Created to Premium Apps list

--- emailscript/script.sh
+++ emailscript/script.sh
@@ -1,6 +1,9 @@
-gc routing queues list --autopaginate --filtercondition="name==Customer Support" --transformstr='{{- range . -}}{{.id}}{{- end -}}'
-
-gc analytics conversations details query create --file=./analyticsQuery.json --inputformat=JSON
-
-
-gc conversations disconnect create 6620f0cf-987d-4587-82ce-c2e3c2ecb54a
+#!/bin/zsh
+gc routing queues list --autopaginate --filtercondition="name==Customer Support" --transform=./analyticsQuery.gotmpl > analyticsQuery.json
+gc analytics conversations details query create --file=./analyticsQuery.json --inputformat=JSON --transformstr="{{- range .conversations -}}{{.conversationId}}\r\n{{- end -}}" > conversationIds.txt
+cat conversationIds.txt | IFS=$'\n' read -r -d '' -A lines
+for line in "${(@f)lines}"
+{
+    echo $line
+    gc conversations disconnect create $line
+}
